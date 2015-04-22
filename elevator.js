cdf7fe720655f7122118ab57b98b5e3b128227eb
Joshua Walsh <josh@ymindustries.com>
2015-04-22 21:46:09 +1000

    Remove TODO notice about tap bindings
    The answer is no. Touch devices send a click event when something is
    tapped.

--- elevator.js
+++ elevator.js
@@ -145,7 +145,6 @@ var Elevator = (function() {
         }
     }
 
-    //@TODO: Does this need tap bindings too?
     function bindElevateToElement( element ) {
         element.addEventListener('click', elevate, false);
     }
