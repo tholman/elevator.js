a26a3494af860a7489e7f376c4a476f143e47e4a
Tim <timothy.w.holman@gmail.com>
2015-04-27 22:08:47 -0400

    move extend params to utils

--- elevator.js
+++ elevator.js
@@ -50,6 +50,16 @@ var Elevator = (function() {
         return -c/2 * (t*(t-2) - 1) + b;
     };
 
+    function extendParameters(options, defaults){
+        for(var option in defaults){
+            var t = options[option] === undefined && typeof option !== "function";
+            if(t){
+                options[option] = defaults[option];
+            }
+        }
+        return options;
+    }
+
     /**
      * Main
      */
@@ -148,16 +158,6 @@ var Elevator = (function() {
     //@TODO: Does this need tap bindings too?
     function bindElevateToElement( element ) {
         element.addEventListener('click', elevate, false);
-    }
-
-    function extendParameters(options, defaults){
-        for(var option in defaults){
-            var t = options[option] === undefined && typeof option !== "function";
-            if(t){
-                options[option] = defaults[option];
-            }
-        }
-        return options;
     }
 
     function main( options ) {
