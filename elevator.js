c54329c161085b7478d69244f793621ba1585b55
Joao Cunha <joao@joaocunha.net>
2015-08-11 19:49:05 -0300

    IIFE wrapping

--- elevator.js
+++ elevator.js
@@ -9,205 +9,207 @@
  * Elevator.js
  *********************************************/
 
-var Elevator = function(options) {
-
+;(function(win) {
   'use strict';
 
-  // Elements
-  var body = null;
+  win.Elevator = function(options) {
 
-  // Scroll vars
-  var animation = null;
-  var duration = null; // ms
-  var customDuration = false;
-  var startTime = null;
-  var startPosition = null;
-  var elevating = false;
+    // Elements
+    var body = null;
 
-  var mainAudio;
-  var endAudio;
+    // Scroll vars
+    var animation = null;
+    var duration = null; // ms
+    var customDuration = false;
+    var startTime = null;
+    var startPosition = null;
+    var elevating = false;
 
-  var that = this;
+    var mainAudio;
+    var endAudio;
 
-  /**
-   * Utils
-   */
+    var that = this;
 
-  // Thanks Mr Penner - http://robertpenner.com/easing/
-  function easeInOutQuad(t, b, c, d) {
-    t /= d / 2;
+    /**
+     * Utils
+     */
 
-    if (t < 1) {
-      return c / 2 * t * t + b;
-    }
+    // Thanks Mr Penner - http://robertpenner.com/easing/
+    function easeInOutQuad(t, b, c, d) {
+      t /= d / 2;
 
-    t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
-  }
-
-  function extendParameters(options, defaults) {
-    for (var option in defaults) {
-      var t = options[option] === undefined && typeof option !== 'function';
-      if (t) {
-        options[option] = defaults[option];
+      if (t < 1) {
+        return c / 2 * t * t + b;
       }
-    }
-    return options;
-  }
 
-  /**
-   * Main
-   */
-
-  // Time is passed through requestAnimationFrame, what a world!
-  function animateLoop(time) {
-    if (!startTime) {
-      startTime = time;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
     }
 
-    var timeSoFar = time - startTime;
-    var easedPosition = easeInOutQuad(timeSoFar, startPosition, -startPosition, duration);
-
-    window.scrollTo(0, easedPosition);
-
-    if (timeSoFar < duration) {
-      animation = requestAnimationFrame(animateLoop);
-    } else {
-      animationFinished();
-    }
-   }
-
-//            ELEVATE!
-//              /
-//         ____
-//       .'    '=====<0
-//       |======|
-//       |======|
-//       [IIIIII[\--()
-//       |_______|
-//       C O O O D
-//      C O  O  O D
-//     C  O  O  O  D
-//     C__O__O__O__D
-//    [_____________]
-  this.elevate = function() {
-
-    if (elevating) {
-      return;
+    function extendParameters(options, defaults) {
+      for (var option in defaults) {
+        var t = options[option] === undefined && typeof option !== 'function';
+        if (t) {
+          options[option] = defaults[option];
+        }
+      }
+      return options;
     }
 
-    elevating = true;
-    startPosition = (document.documentElement.scrollTop || body.scrollTop);
+    /**
+     * Main
+     */
 
-    // No custom duration set, so we travel at pixels per millisecond. (0.75px per ms)
-    if (!customDuration) {
-      duration = (startPosition * 1.5);
+    // Time is passed through requestAnimationFrame, what a world!
+    function animateLoop(time) {
+      if (!startTime) {
+        startTime = time;
+      }
+
+      var timeSoFar = time - startTime;
+      var easedPosition = easeInOutQuad(timeSoFar, startPosition, -startPosition, duration);
+
+      window.scrollTo(0, easedPosition);
+
+      if (timeSoFar < duration) {
+        animation = requestAnimationFrame(animateLoop);
+      } else {
+        animationFinished();
+      }
+     }
+
+  //            ELEVATE!
+  //              /
+  //         ____
+  //       .'    '=====<0
+  //       |======|
+  //       |======|
+  //       [IIIIII[\--()
+  //       |_______|
+  //       C O O O D
+  //      C O  O  O D
+  //     C  O  O  O  D
+  //     C__O__O__O__D
+  //    [_____________]
+    this.elevate = function() {
+
+      if (elevating) {
+        return;
+      }
+
+      elevating = true;
+      startPosition = (document.documentElement.scrollTop || body.scrollTop);
+
+      // No custom duration set, so we travel at pixels per millisecond. (0.75px per ms)
+      if (!customDuration) {
+        duration = (startPosition * 1.5);
+      }
+
+      requestAnimationFrame(animateLoop);
+
+      // Start music!
+      if (mainAudio) {
+        mainAudio.play();
+      }
+    };
+
+    function browserMeetsRequirements() {
+      return window.requestAnimationFrame && window.Audio && window.addEventListener;
     }
 
-    requestAnimationFrame(animateLoop);
-
-    // Start music!
-    if (mainAudio) {
-      mainAudio.play();
-    }
-  };
-
-  function browserMeetsRequirements() {
-    return window.requestAnimationFrame && window.Audio && window.addEventListener;
-  }
-
-  function resetPositions() {
-    startTime = null;
-    startPosition = null;
-    elevating = false;
-  }
-
-  function animationFinished() {
-
-    resetPositions();
-
-    // Stop music!
-    if (mainAudio) {
-      mainAudio.pause();
-      mainAudio.currentTime = 0;
+    function resetPositions() {
+      startTime = null;
+      startPosition = null;
+      elevating = false;
     }
 
-    if (endAudio) {
-      endAudio.play();
-    }
-  }
+    function animationFinished() {
 
-  function onWindowBlur() {
-
-    // If animating, go straight to the top. And play no more music.
-    if (elevating) {
-
-      cancelAnimationFrame(animation);
       resetPositions();
 
+      // Stop music!
       if (mainAudio) {
         mainAudio.pause();
         mainAudio.currentTime = 0;
       }
 
-      window.scrollTo(0, 0);
-    }
-  }
-
-  function bindElevateToElement(element) {
-    if (element.addEventListener) {
-      element.addEventListener('click', that.elevate, false);
-    } else {
-      // Older browsers
-      element.attachEvent('onclick', function() {
-        document.documentElement.scrollTop = 0;
-        document.body.scrollTop = 0;
-        window.scroll(0, 0);
-      });
-    }
-  }
-
-  function init(_options) {
-    // Bind to element click event, if need be.
-    body = document.body;
-
-    var defaults = {
-      duration: undefined,
-      mainAudio: false,
-      endAudio: false,
-      preloadAudio: true,
-      loopAudio: true,
-    };
-
-    _options = extendParameters(_options, defaults);
-
-    if (_options.element) {
-      bindElevateToElement(_options.element);
+      if (endAudio) {
+        endAudio.play();
+      }
     }
 
-    // Take the stairs instead
-    if (!browserMeetsRequirements()) {
-      return;
+    function onWindowBlur() {
+
+      // If animating, go straight to the top. And play no more music.
+      if (elevating) {
+
+        cancelAnimationFrame(animation);
+        resetPositions();
+
+        if (mainAudio) {
+          mainAudio.pause();
+          mainAudio.currentTime = 0;
+        }
+
+        window.scrollTo(0, 0);
+      }
     }
 
-    if (_options.duration) {
-      customDuration = true;
-      duration = _options.duration;
+    function bindElevateToElement(element) {
+      if (element.addEventListener) {
+        element.addEventListener('click', that.elevate, false);
+      } else {
+        // Older browsers
+        element.attachEvent('onclick', function() {
+          document.documentElement.scrollTop = 0;
+          document.body.scrollTop = 0;
+          window.scroll(0, 0);
+        });
+      }
     }
 
-    window.addEventListener('blur', onWindowBlur, false);
+    function init(_options) {
+      // Bind to element click event, if need be.
+      body = document.body;
 
-    if (_options.mainAudio) {
-      mainAudio = new Audio(_options.mainAudio);
-      mainAudio.setAttribute('preload', _options.preloadAudio);
-      mainAudio.setAttribute('loop', _options.loopAudio);
+      var defaults = {
+        duration: undefined,
+        mainAudio: false,
+        endAudio: false,
+        preloadAudio: true,
+        loopAudio: true,
+      };
+
+      _options = extendParameters(_options, defaults);
+
+      if (_options.element) {
+        bindElevateToElement(_options.element);
+      }
+
+      // Take the stairs instead
+      if (!browserMeetsRequirements()) {
+        return;
+      }
+
+      if (_options.duration) {
+        customDuration = true;
+        duration = _options.duration;
+      }
+
+      window.addEventListener('blur', onWindowBlur, false);
+
+      if (_options.mainAudio) {
+        mainAudio = new Audio(_options.mainAudio);
+        mainAudio.setAttribute('preload', _options.preloadAudio);
+        mainAudio.setAttribute('loop', _options.loopAudio);
+      }
+
+      if (_options.endAudio) {
+        endAudio = new Audio(_options.endAudio);
+        endAudio.setAttribute('preload', 'true');
+      }
     }
 
-    if (_options.endAudio) {
-      endAudio = new Audio(_options.endAudio);
-      endAudio.setAttribute('preload', 'true');
-    }
-  }
-
-  init(options);
-};
+    init(options);
+  };
+})(window);
